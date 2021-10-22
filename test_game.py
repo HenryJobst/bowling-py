299d43cf4e278ad02ac70a3afefb20d494e26ea1
Henry Jobst <henry@jobst-berlin.de>
2021-10-22 23:19:34 +0200

    code cleanup

--- test_game.py
+++ test_game.py
@@ -12,20 +12,17 @@ class GameTestCase(unittest.TestCase):
         self.game.roll_many(20, 0)
         self.assertEqual(0, self.game.score())
 
-
     def test_all_one_game(self):
         self.game.roll_many(20, 1)
         self.assertEqual(20, self.game.score())
 
     def test_one_valueless_spare(self):
-        self.game.roll(5)
-        self.game.roll(5)  # spare
+        self.game.roll_many(2, 5)  # spare
         self.game.roll_many(18, 0)
         self.assertEquals(10, self.game.score())
 
     def test_one_spare(self):
-        self.game.roll(5)
-        self.game.roll(5)  # spare
+        self.game.roll_many(2, 5)  # spare
         self.game.roll(3)
         self.game.roll_many(17, 0)
         self.assertEquals(16, self.game.score())
@@ -37,21 +34,17 @@ class GameTestCase(unittest.TestCase):
 
     def test_one_strike(self):
         self.game.roll(10)  # strike
-        self.game.roll(3)
-        self.game.roll(3)
+        self.game.roll_many(2, 3)
         self.game.roll_many(16, 0)
         self.assertEquals(22, self.game.score())
 
     def test_double_strike(self):
-        self.game.roll(10)  # strike
-        self.game.roll(10)  # strike
+        self.game.roll_many(2, 10)  # double strike
         self.game.roll_many(16, 0)
         self.assertEquals(40, self.game.score())
 
     def test_tripple_strike(self):
-        self.game.roll(10)
-        self.game.roll(10)
-        self.game.roll(10)
+        self.game.roll_many(3, 10)  # tripple strike
         self.game.roll_many(14, 0)
         self.assertEquals(70, self.game.score())
 
